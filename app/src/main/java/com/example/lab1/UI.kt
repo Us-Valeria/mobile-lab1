92bba8ff527eeea751671daf8b5fa6fe6ea90194
plesnyarskaya <plesnyarskayarina@yandex.ru>
2024-09-18 23:40:16 +0700

    add

--- app/src/main/java/com/example/lab1/UI.kt
+++ app/src/main/java/com/example/lab1/UI.kt
@@ -1,9 +1,11 @@
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab1.models.News
@@ -11,31 +13,58 @@ import com.example.lab1.models.News
 @Composable
 fun NewsScreen(newsViewModel: NewsViewModel = viewModel()) {
     val newsList = newsViewModel.newsList
-
-    Column(modifier = Modifier.fillMaxSize()) {
-        Row(modifier = Modifier.weight(1f)) {
-            NewsCard(news = newsList[0], onLike = {newsViewModel.increaseLikes(it)}, Modifier.weight(1f))
-            NewsCard(news = newsList[1], onLike = {newsViewModel.increaseLikes(it)}, Modifier.weight(1f))
-        }
-        Row(modifier = Modifier.weight(1f)) {
-            NewsCard(news = newsList[2], onLike = {newsViewModel.increaseLikes(it)}, Modifier.weight(1f))
-            NewsCard(news = newsList[3], onLike = {newsViewModel.increaseLikes(it)}, Modifier.weight(1f))
+    Box(
+        modifier = Modifier.padding(8.dp)
+            .fillMaxSize()
+            .background(Color(0xFF8000F))
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row(modifier = Modifier.weight(1f)) {
+                NewsCard(
+                    news = newsList[0],
+                    onLike = { newsViewModel.increaseLikes(it) },
+                    Modifier.weight(1f)
+                )
+                NewsCard(
+                    news = newsList[1],
+                    onLike = { newsViewModel.increaseLikes(it) },
+                    Modifier.weight(1f)
+                )
+            }
+            Row(modifier = Modifier.weight(1f)) {
+                NewsCard(
+                    news = newsList[2],
+                    onLike = { newsViewModel.increaseLikes(it) },
+                    Modifier.weight(1f)
+                )
+                NewsCard(
+                    news = newsList[3],
+                    onLike = { newsViewModel.increaseLikes(it) },
+                    Modifier.weight(1f)
+                )
+            }
         }
     }
 }
 
 @Composable
 fun NewsCard(news: News, onLike: (News) -> Unit, modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier.padding(8.dp)
+    Card(
+        modifier = modifier
+            .padding(8.dp)
+            .clickable { onLike(news) },
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
             verticalArrangement = Arrangement.SpaceBetween
+
         ) {
             Text(text = news.title, modifier = Modifier.weight(0.9f))
             Text(
                 text = "${news.likes}",
+                style = MaterialTheme.typography.bodySmall,
                 modifier = Modifier
                     .weight(0.1f)
                     .clickable { onLike(news) }
@@ -47,5 +76,7 @@ fun NewsCard(news: News, onLike: (News) -> Unit, modifier: Modifier = Modifier) 
 @Preview(showBackground = true)
 @Composable
 fun NewsScreenPreview() {
-    NewsScreen()
+    MaterialTheme {
+        NewsScreen()
+    }
 }
