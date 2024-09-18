31f2e3483696b5330d1d46cee12ff86fb77b4e25
Valeria Granina <v_granina.04@mail.ru>
2024-09-18 20:31:37 +0700

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,9 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.compose.foundation:foundation:1.5.3")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
