06172da08867d4fa2b78afaa735dc9b0e32c87bf
Valeria Granina <v_granina.04@mail.ru>
2024-09-18 20:51:17 +0700

    add NewsViewModal and ui

---
+++ app/src/main/java/com/example/lab1/NewsViewModel.kt
@@ -0,0 +1,32 @@
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.lab1.models.News
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class NewsViewModel : ViewModel() {
+
+    val newsList = mutableStateListOf(
+        News(1, "Новость 1"),
+        News(2, "Новость 2"),
+        News(3, "Новость 3"),
+        News(4, "Новость 4"),
+        News(5, "Новость 5"),
+        News(6, "Новость 6"),
+        News(7, "Новость 7"),
+        News(8, "Новость 8"),
+        News(9, "Новость 9"),
+        News(10, "Новость 10")
+    )
+
+    init {
+        viewModelScope.launch {
+            while (true) {
+                delay(5000)
+                replaceRandomNews()
+            }
+        }
+    }
+    private fun replaceRandomNews() {}
+}
---
+++ app/src/main/java/com/example/lab1/UI.kt
@@ -0,0 +1,48 @@
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab1.models.News
+
+@Composable
+fun NewsScreen(newsViewModel: NewsViewModel = viewModel()) {
+    val newsList = newsViewModel.newsList
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.weight(1f)) {
+            NewsCard(news = newsList[0], onLike = {}, Modifier.weight(1f))
+            NewsCard(news = newsList[1], onLike = {}, Modifier.weight(1f))
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            NewsCard(news = newsList[2], onLike = {}, Modifier.weight(1f))
+            NewsCard(news = newsList[3], onLike = {}, Modifier.weight(1f))
+        }
+    }
+}
+
+@Composable
+fun NewsCard(news: News, onLike: (News) -> Unit, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = news.title, modifier = Modifier.weight(0.9f))
+            Text(
+                text = "${news.likes}",
+                modifier = Modifier.weight(0.1f)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NewsScreenPreview() {
+    NewsScreen()
+}
